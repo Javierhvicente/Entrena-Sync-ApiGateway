a1d339cb00b327913e39ad2729f772b47919ad3f
Javier Hernández vicente <javier.hernandez@alumno.iesluisvives.org>
2025-03-14 18:25:09 +0100

    init

---
+++ src/main/java/entrenasync/dev/entrenasyncapigateway/EntrenaSyncApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package entrenasync.dev.entrenasyncapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrenaSyncApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntrenaSyncApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/entrenasync/dev/entrenasyncapigateway/EntrenaSyncApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package entrenasync.dev.entrenasyncapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrenaSyncApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
