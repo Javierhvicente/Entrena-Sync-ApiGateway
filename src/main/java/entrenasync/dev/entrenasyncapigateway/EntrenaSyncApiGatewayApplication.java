28110bdd0a12dcc16846bc50306a11f94e32f6eb
Javier Hernández vicente <javier.hernandez@alumno.iesluisvives.org>
2025-03-17 18:03:11 +0100

    dummy commit

--- src/main/java/entrenasync/dev/entrenasyncapigateway/EntrenaSyncApiGatewayApplication.java
+++ src/main/java/entrenasync/dev/entrenasyncapigateway/EntrenaSyncApiGatewayApplication.java
@@ -1,14 +1,18 @@
 package entrenasync.dev.entrenasyncapigateway;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EntrenaSyncApiGatewayApplication {
+public class EntrenaSyncApiGatewayApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(EntrenaSyncApiGatewayApplication.class, args);
 
     }
-
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("🟢 Servidor escuchando");
+    }
 }
