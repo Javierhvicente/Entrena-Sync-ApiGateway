b77ba1f295180b17e026afe286d1e499e41a8f8f
Álvaro Herero <alvaro.herrero11x@gmail.com>
2025-04-26 14:11:54 +0200

    Refactors and configures authentication services
    Simplifies authentication service implementation by removing unnecessary interfaces and renames concrete classes for better clarity.
    Adds a Dockerfile for the development environment.
    Updates application properties to align with the new configuration.

--- src/main/java/entrenasync/dev/entrenasyncapigateway/Auth/Services/ISessionService.java
+++
@@ -1,9 +0,0 @@
-package entrenasync.dev.entrenasyncapigateway.Auth.Services;
-
-import entrenasync.dev.entrenasyncapigateway.Auth.dto.LoginRequest;
-import entrenasync.dev.entrenasyncapigateway.Auth.dto.LoginResponse;
-import reactor.core.publisher.Mono;
-
-public interface ISessionService {
-    Mono<LoginResponse> login(LoginRequest request);
-}
