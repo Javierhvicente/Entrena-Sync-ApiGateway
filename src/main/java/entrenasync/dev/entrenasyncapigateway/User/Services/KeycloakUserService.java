f149792106c5eb53052e608251ca0b2b82571695
Javier Hernández vicente <javier.hernandez@alumno.iesluisvives.org>
2025-06-01 20:44:55 +0200

    FIX: Update user con los campos que quieras

--- src/main/java/entrenasync/dev/entrenasyncapigateway/User/Services/KeycloakUserService.java
+++ src/main/java/entrenasync/dev/entrenasyncapigateway/User/Services/KeycloakUserService.java
@@ -2,6 +2,7 @@ package entrenasync.dev.entrenasyncapigateway.User.Services;
 
 import entrenasync.dev.entrenasyncapigateway.User.Dto.UserRequest;
 import entrenasync.dev.entrenasyncapigateway.User.Dto.UserResponse;
+import entrenasync.dev.entrenasyncapigateway.User.Dto.UserUpdateRequest;
 import entrenasync.dev.entrenasyncapigateway.Utils.PagedResponse;
 
 import java.util.List;
@@ -9,6 +10,6 @@ public interface KeycloakUserService {
     PagedResponse<UserResponse> getAllUsers(int page, int size);
     UserResponse getUserByUsername(String username);
     UserResponse createUser(UserRequest userRequest);
-    UserResponse updateUser(UserRequest userRequest, String userId);
+    UserResponse updateUser(UserUpdateRequest userRequest, String userId);
     void deleteUser(String userId);
 }
